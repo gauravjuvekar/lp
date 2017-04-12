eb308c467f82215bccf9395f8149c59be6252b72
Gaurav Juvekar <gauravjuvekar@gmail.com>
2017-04-12 23:51:05 +0530

    Allow reading from a file

--- nl/nl.c
+++ nl/nl.c
@@ -1,14 +1,29 @@
 #define _GNU_SOURCE 1
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
-	(void) argc;
-	(void) argv;
+	FILE *file;
+	if (argc == 1) {
+		file = stdin;
+	}
+	else if (argc == 2) {
+		file = fopen(argv[1], "r");
+		if (file == NULL) {
+			fprintf(stderr, "%s\n", strerror(errno));
+			exit(2);
+		}
+	}
+	else {
+		fprintf(stderr, "Usage: nl [FILE]");
+		exit(1);
+	}
 
 	char *line = NULL;
 	size_t size = 0;
-	for(size_t line_num = 1; getline(&line, &size, stdin) >= 0; line_num++) {
+	for(size_t line_num = 1; getline(&line, &size, file) >= 0; line_num++) {
 		printf("%8zu: %s", line_num, line);
 	}
 	free(line);
